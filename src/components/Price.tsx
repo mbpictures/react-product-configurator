39e2ed89b8d59ad63a730869f4ff4054183555e7
Marius Butz <mbpictures13@gmail.com>
2020-10-01 21:38:41 +0200

    convert price to class component and add change animation

--- src/components/Price.tsx
+++ src/components/Price.tsx
@@ -6,11 +6,39 @@ interface props {
     price: number;
 }
 
-export function Price(props: props) {
-    return (
-        <div className={style.price}>
-            <span>Price:</span>
-            <span>{props.price} &euro;</span>
-        </div>
-    );
+export class Price extends React.Component<props> {
+    priceElem: React.RefObject<any>;
+    constructor(props: props) {
+        super(props);
+        this.state = {
+            className: "",
+        };
+
+        this.priceElem = React.createRef();
+        this.removeAnimationClass = this.removeAnimationClass.bind(this);
+    }
+
+    componentDidUpdate(prevProps: Readonly<props>) {
+        if (prevProps.price !== this.props.price) {
+            this.priceElem.current.classList.add(style.play);
+        }
+    }
+
+    removeAnimationClass() {
+        this.priceElem.current.classList.remove(style.play);
+    }
+
+    render() {
+        return (
+            <div className={style.price}>
+                <span>Price:</span>
+                <span
+                    ref={this.priceElem}
+                    onAnimationEnd={this.removeAnimationClass}
+                >
+                    {this.props.price} &euro;
+                </span>
+            </div>
+        );
+    }
 }
